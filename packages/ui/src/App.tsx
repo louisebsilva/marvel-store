7eff9c8001f968e8457f6774d08248fd0a60444a
Louise Bahiense <louisebahienser@gmail.com>
2021-03-20 20:21:14 -0300

    Add search and cardList features

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import CardList from './CardList/CardList';
+import Header from './header/Header';
+import mockData from './mock.json';
 
-function App() {
+const options = [ 'Comic', 'Character' ];
+
+const App: React.FC = () => {
+  const [ searchInput, setSearchInput ] = useState('');
+  const [ searchOptions, setSearchOptions ] = useState(options[0]);
+  const [ list, setList ] = useState([]);
+
+  const search = (e: any) => {
+    e.preventDefault();
+    console.log('Search!');
+    setSearchInput('');
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    setList(mockData.results as any);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <form>
+        <select name="searchType" onChange={(e) => setSearchOptions(e.target.value)}>
+          { options.map((x, index) => <option value={x} key={index}>{x}</option>) }
+        </select>
+        <input type="text" id="searchInput" name="searchInput" onChange={(e) => setSearchInput(e.target.value)} placeholder="Digite o que deseja encontrar" value={searchInput} required />
+        <button disabled={searchInput.length === 0} onClick={(e) => search(e)}>Buscar</button>
+      </form>
+      <CardList cardData={list} />
     </div>
   );
-}
+};
 
 export default App;
